31a21fb915ce3e155f65f934bb2f391d6c13a14d
jimmg35 <jim60308@gmail.com>
2025-05-14 10:01:53 +0800

    feat: add navbar icon

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,6 +1,6 @@
 const Welcome = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-500">
       <h1 className="text-4xl font-bold">Welcome to the SDSC!</h1>
       <p className="mt-4 text-lg">
         Your journey into spatial data science begins here.
