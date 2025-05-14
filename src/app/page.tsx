f2b7319ff34c086dabcc5665bcce979ed79a5a80
jimmg35 <jim60308@gmail.com>
2025-05-14 10:56:51 +0800

    feat: complete welcome secion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import Welcome from "@/components/Welcome";
 
 export default function Home() {
   return (
     <div className="h-fit">
       <Welcome />
-      <Welcome />
+      {/* <Welcome /> */}
     </div>
   );
 }
--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,10 +1,43 @@
+import Link from "next/link";
+import { Telescope } from "lucide-react";
+
 const Welcome = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-400">
-      <h1 className="text-4xl font-bold">Welcome to the SDSC!</h1>
-      <p className="mt-4 text-lg">
-        Your journey into spatial data science begins here.
-      </p>
+    <div className="relative w-full h-screen overflow-hidden p-4">
+      {/* bg video */}
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10 backdrop-blur-sm"
+      >
+        <source src="/vid/welcome.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      {/* gray filter for vid */}
+      <div className="absolute top-0 left-0 w-full h-full bg-gray-800 opacity-50 -z-5"></div>
+
+      {/* welcome text */}
+      <div className="flex flex-col items-center justify-center h-full relative z-10 text-white">
+        <div className="bg-teal-500 p-4 rounded-lg flex flex-col gap-4">
+          <h1 className="text-3xl font-bold">
+            Welcome to Spatial Data Science Center!
+          </h1>
+          <div className="flex">
+            <p className="text-xl">Your journey into GIScience begins here.</p>
+
+            <Link
+              href={"/about"}
+              className={`hover:bg-white hover:text-teal-400 transition ml-4 pt-2 pb-2 flex items-center gap-1 border-2 border-white pr-2 rounded-md`}
+            >
+              <Telescope size={24} className="ml-2" />
+              Explore
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
