8f5c10af6d4b5ddd04a43ff8140ce07f83fd662b
jimmg35 <jim60308@gmail.com>
2025-05-14 10:21:58 +0800

    feat: add rwd nav menu

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -9,6 +9,8 @@ import {
   CalendarDays,
   Rss,
   PhoneCall,
+  Menu,
+  CircleX,
 } from "lucide-react";
 
 const NavDirection = [
@@ -44,14 +46,14 @@ function Navbar() {
         flex items-center
         ${
           isScrolled
-            ? "bg-white text-black h-[80px]"
+            ? "bg-white text-black h-[80px] shadow-md"
             : "bg-transparent text-white h-[110px]"
         }
       `}
     >
       <div className="container mx-auto flex items-center justify-between px-4 py-2">
         {/* Logo */}
-        <div className="text-3xl font-bold transition">SDSC</div>
+        <div className="text-teal-400 text-3xl font-bold transition">SDSC</div>
 
         {/* Desktop Navigation */}
         <ul className="hidden md:flex gap-8">
@@ -71,50 +73,45 @@ function Navbar() {
         {/* Burger Menu Button */}
         <div className="md:hidden">
           <button
-            onClick={(prev) => {
-              setIsOpen(!prev);
-              console.log("wwww");
+            onClick={() => {
+              setIsOpen((prev) => !prev);
             }}
             className="p-2 rounded-md focus:outline-none hover:bg-gray-700"
             aria-label="Toggle Navigation"
           >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
-              />
-            </svg>
+            <Menu size={24} />
           </button>
         </div>
-
-        {/* Mobile Dropdown Menu */}
-        {isOpen && (
-          <div>asdasdsa</div>
-          // <div>
-          //   <ul className="flex flex-col gap-2 p-4 bg-gray-800">
-          //     {NavDirection.map((item) => (
-          //       <li key={item.name}>
-          //         <Link
-          //           href={item.href}
-          //           className="block px-4 py-2 text-base transition hover:bg-gray-700"
-          //           onClick={() => setIsOpen(false)}
-          //         >
-          //           {item.name}
-          //         </Link>
-          //       </li>
-          //     ))}
-          //   </ul>
-          // </div>
-        )}
       </div>
+
+      {/* Mobile Dropdown Menu */}
+      {isOpen && (
+        <div className="absolute top-0 left-0 w-screen h-screen bg-gray-800 text-white">
+          <div className="pt-8 pr-8 pl-8 pb-4 flex justify-end">
+            <button
+              onClick={() => {
+                setIsOpen(false);
+              }}
+            >
+              <CircleX size={28} />
+            </button>
+          </div>
+          <ul className="flex flex-col gap-2 p-4 ">
+            {NavDirection.map((item) => (
+              <li key={item.name}>
+                <Link
+                  href={item.href}
+                  className="flex items-center gap-1 px-4 py-2 text-base transition hover:bg-gray-700"
+                  onClick={() => setIsOpen(false)}
+                >
+                  {item.icon}
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
