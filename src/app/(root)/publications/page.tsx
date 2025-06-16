1db2943cb520627de442b524c466e1a07c126721
jimmg35 <jim60308@gmail.com>
2025-06-16 16:48:29 +0800

    feat: update publication page

---
+++ src/app/(root)/publications/page.tsx
@@ -0,0 +1,30 @@
+import Banner from '@/components/Utility/Banner';
+import ProfileCard from '@/components/Utility/ProfileCard';
+import PublicationPost from '@/components/Utility/PublicationPost';
+import Search from '@/components/Utility/Search';
+import { PublicationData, getAllPublications } from '@/lib/publications';
+// import { MemberData, getAllMembers } from '@/lib/members';
+import { ScrollText, UserRound } from 'lucide-react';
+
+export default function Publications() {
+  const publications: PublicationData[] = getAllPublications();
+  // const members: MemberData[] = getAllMembers();
+
+  return (
+    <section className="relative min-h-dvh">
+      <Banner
+        title="Publications"
+        imageUrl="/img/banners/publications-banner.jpg"
+        icon={<ScrollText size={52} className="inline-block mr-2" />}
+      />
+      <div className="mx-auto max-w-7xl pt-10 px-6">
+        <Search placeholder="Search for publications!" />
+      </div>
+      <div className="mx-auto max-w-7xl py-10 px-6 flex flex-wrap items-center justify-center gap-8">
+        {publications.map((publication) => (
+          <PublicationPost key={publication.id} {...publication} />
+        ))}
+      </div>
+    </section>
+  );
+}
