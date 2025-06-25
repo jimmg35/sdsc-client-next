c303134610c878fe6b586f3a5db2a888ac2c3c7d
MSI\user <jimmg350@gmail.com>
2025-06-25 21:43:11 +0800

    feat: update

--- src/components/Home/WhoWeAre/index.tsx
+++ src/components/Home/WhoWeAre/index.tsx
@@ -1,4 +1,5 @@
 import { Atom, FolderCode, Microscope } from 'lucide-react';
+import { Fade, Slide } from 'react-awesome-reveal';
 
 const Circle = ({
   text,
@@ -19,27 +20,29 @@ const Circle = ({
 const WhoWeAre = () => {
   return (
     <div className="relative w-full h-fit overflow-hidden px-4 py-12 flex flex-col items-center">
-      <h1 className="text-4xl md:text-6xl font-bold">Who We Are</h1>
-      <p className="mt-4 text-xl mx-4 md:mx-84 text-center">
-        The SDSC serves as a platform where anyone with spatial analysis needs
-        can turn to us for support. We also provide a range of open-source
-        spatial software for use.
-      </p>
-      <div className="flex gap-8 py-8 justify-center flex-wrap">
-        <Circle text="Spatial Data Science">
-          <Atom size={48} className="text-teal-400" />
-        </Circle>
-        <Circle text="MGWR Software">
-          <FolderCode size={48} className="text-teal-400" />
-        </Circle>
-        <Circle text="Interdisciplinary Research">
-          <Microscope size={48} className="text-teal-400" />
-        </Circle>
-      </div>
-      <p className="mt-4 text-xl mx-4 md:mx-84 text-center font-bold">
-        Empowering spatial understanding through cutting-edge GIScience
-        research.
-      </p>
+      <Fade direction="up" duration={500} cascade triggerOnce>
+        <h1 className="text-4xl md:text-6xl font-bold">Who We Are</h1>
+        <p className="mt-4 text-xl mx-4 md:mx-84 text-center">
+          The SDSC serves as a platform where anyone with spatial analysis needs
+          can turn to us for support. We also provide a range of open-source
+          spatial software for use.
+        </p>
+        <div className="flex gap-8 py-8 justify-center flex-wrap">
+          <Circle text="Spatial Data Science">
+            <Atom size={48} className="text-teal-400" />
+          </Circle>
+          <Circle text="MGWR Software">
+            <FolderCode size={48} className="text-teal-400" />
+          </Circle>
+          <Circle text="Interdisciplinary Research">
+            <Microscope size={48} className="text-teal-400" />
+          </Circle>
+        </div>
+        <p className="mt-4 text-xl mx-4 md:mx-84 text-center font-bold">
+          Empowering spatial understanding through cutting-edge GIScience
+          research.
+        </p>
+      </Fade>
     </div>
   );
 };
